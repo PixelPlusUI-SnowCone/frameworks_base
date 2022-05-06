f7cb748566a554f5ed61fc60c4ba19f488b3229c
LuK1337 <priv.luk@gmail.com>
2022-05-06 21:26:57 +0900

    core: Add ENABLE_TASKBAR setting

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5712,6 +5712,12 @@ public final class Settings {
         public static final String DISPLAY_CUTOUT_HIDDEN = "display_cutout_hidden";
 
         /**
+         * Whether to enable taskbar.
+         * @hide
+         */
+        public static final String ENABLE_TASKBAR = "enable_taskbar";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
